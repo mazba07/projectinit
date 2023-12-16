ac1c97c661342131f17e9745e9b04a9a65747b6c
Mazba Uddin <mazba09@gmail.com>
2023-12-16 17:58:53 +0100

    add in server

---
+++ ci3_for_web_dev/application/views/partials/css_and_js.php
@@ -0,0 +1,32 @@
+<link rel="shortcut icon"
+      href="http://via.placeholder.com/32x32"
+      sizes="32x32"/>
+<link rel="icon" href="http://via.placeholder.com/32x32"
+      sizes="32x32"/>
+<link rel="icon" href="http://via.placeholder.com/192x192"
+      sizes="192x192"/>
+<link rel="apple-touch-icon-precomposed"
+      href="http://via.placeholder.com/120x120"/>
+
+
+<link href="<?php echo base_url('public/resources/plugin/bootstrap-5.2.0-dist/css/bootstrap.min.css') ?>"
+      rel="stylesheet">
+<link href="<?php echo base_url('public/resources/plugin/font-awesome-6-pro-main/css/all.min.css') ?>"
+      rel="stylesheet">
+<link href="<?php echo base_url('public/resources/css/core-style.css') ?>" rel="stylesheet">
+
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+<script>
+    var baseUrl = "<?php echo base_url()?>";
+</script>
+
+<script src="<?php echo base_url('public/resources/plugin/jquery-3.7.0-dist/jquery-3.7.0.min.js') ?>"></script>
+<script src="<?php echo base_url('public/resources/plugin/bootstrap-5.2.0-dist/js/bootstrap.bundle.min.js') ?>"></script>
+
+<script src="<?php echo base_url('public/resources/js/core-script.js') ?>"></script>
